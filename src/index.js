0f4a47d94b8f178bd293928bd7dba7e6393ebd2c
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2021-12-30 16:35:34 +0545

    Netflix Clone Model Completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Card from "./Cards";
+import "./index.css";
+import data from "./Data.jsx";
+
+function nf(val) {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.imgsrc}
+    />
+  );
+}
+
 ReactDOM.render(
   <>
-    <App />
+    <h1 className="heading_style">List of my Best Netflix Series Till Now!</h1>
+    {data.map(nf)};
+    <footer></footer>
   </>,
   document.getElementById("root")
 );
